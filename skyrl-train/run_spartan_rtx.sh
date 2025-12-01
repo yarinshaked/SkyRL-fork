a812b70acf1a7b2caefc466a2e616b129cd6ca6c
Yarin Shaked <yarin.shaked7@gmail.com>
2025-12-01 13:41:32 +0000

    fix paths and metrics being constant by making num_inference_engines be num_gpus and the tensor parallel being 1

--- skyrl-train/run_spartan_rtx.sh
+++ skyrl-train/run_spartan_rtx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 NUM_GPUS=$(echo "$CUDA_VISIBLE_DEVICES" | tr ',' '\n' | wc -l)
 SERVER_NAME="RTX6000"
 EVAL_BATCH_SIZE=1024
