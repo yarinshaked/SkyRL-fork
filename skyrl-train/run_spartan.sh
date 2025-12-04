fb416d1c37bc79f963116301e7e252313003e1ce
Yarin Shaked <yarin_shaked7@mm-lab01.lnx.biu.ac.il>
2025-12-04 08:45:48 +0200

    docker worked for l40

--- skyrl-train/run_spartan.sh
+++ skyrl-train/run_spartan.sh
@@ -10,13 +10,13 @@
 #SBATCH --mail-type=ALL
 
 
-TRAIN_NUM_SEGMENTS=1
-VAL_NUM_SEGMENTS=1
+TRAIN_NUM_SEGMENTS=12
+VAL_NUM_SEGMENTS=6
 
 DATA_SIZE=$((50 * TRAIN_NUM_SEGMENTS))
 
-MICRO_FORWARD_BATCH_SIZE_PER_GPU=1
-MICRO_TRAIN_BATCH_SIZE_PER_GPU=1
+MICRO_FORWARD_BATCH_SIZE_PER_GPU=4
+MICRO_TRAIN_BATCH_SIZE_PER_GPU=4
 POLICY_MINI_BATCH_SIZE=$((MICRO_TRAIN_BATCH_SIZE_PER_GPU * NUM_GPUS))
 GLOBAL_TRAIN_BATCH_SIZE=$((POLICY_MINI_BATCH_SIZE * 1))
 TARGET_NUM_STEPS=1000
@@ -86,7 +86,7 @@ uv run --isolated --extra vllm -m $TRAINER \
   trainer.resume_mode=$CHECKPOINT \
   trainer.ckpt_path="${DATA_DIR}/checkpoints/${RUN_NAME}" \
   trainer.max_ckpts_to_keep=3 \
-  trainer.ckpt_interval=1 \
+  trainer.ckpt_interval=5 \
   trainer.export_path="${DATA_DIR}/exports/${RUN_NAME}" \
   trainer.logger="wandb" \
   trainer.project_name="skyrl" \
